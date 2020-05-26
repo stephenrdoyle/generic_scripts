49585b14cf0ea4a0f27e63c0f1a6559185f3f789
“stephenrdoyle” <sd21@sanger.ac.uk>
2020-05-27 00:25:11 +0100

    general_update

--- playtime.sh
+++ playtime.sh
@@ -2,286 +2,126 @@
 # playtime
 
 
+#install.packages("ggmsa")
+#install.packages("seqmagick")
+## ## loading the package
+library("ggmsa")
+library(seqmagick)
 
 
-#--------------------------------------------------------------------------------
-# Figure 2 - Fst plot for parent strains
-
-working dir:
-cd /nfs/users/nfs_s/sd21/lustre118_link/hc/XQTL/04_VARIANTS/PARENTS
-
-parents <- read.table("XQTL_PARENTS.merged.fst",header=F)
-parents <- parents[parents$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-parents <- dplyr::select(parents, V1, V2, V7)
-colnames(parents) <- c("CHR","POS","FST")
-parents$LABEL <- "P0: MHco3(ISE) vs MHco18(UGA)"
-parents$ROW_ID <- 1:nrow(parents)
-colnames(parents) <- c("CHR","POS","FST","LABEL","ROW_ID")
-
-data <- parents
-
-# make the parental plot
-
-# genome wide signficance per sample
-data_gws <- data %>%
-    group_by(LABEL) %>%
-    summarise(GWS = mean(FST)+3*sd(FST))
+sequences <- system.file("extdata", "wbp_acr8.aln",)
+print(sequences)
 
 
-plot_a <- ggplot(data)+
-     geom_hline(data = data_gws, aes(yintercept=GWS), linetype="dashed",col="black")+
-     geom_point(aes(ROW_ID*5000, FST, colour = CHR, group=LABEL),size=0.1)+
-     ylim(0,1)+xlim(0,300e6)+
-     labs(title="A",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8),limits = c(0,300e6))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
+ggmsa( "test", 100, 200, font = NULL, color = "Chemistry_AA")
+
+
+WBGene00036931|cabrigprjna10731   Caenorhabditis_briggsae
+Cni-acr-8|canigoprjna384657  Caenorhabditis_nigoni
+WBGene00058468|caremaprjna53967   Caenorhabditis_remanei
+Csp11.Scaffold629.g13743|catropprjna53597   Caenorhabditis_tropicalis
+ACAC_0000398801|ancantprjeb493    Angiostrongylus_cantonensis
+ANCDUO_01121|anduodprjna72581     Ancylostoma_duodenale
+ACOC_0000247401|ancostprjeb494    Angiostrongylus_costaricensis
+Cang_2012_03_13_00065.g3190|caangaprjna51225     Caenorhabditis_angaria
+ANCCAN_17789|ancaniprjna72585     Ancylostoma_caninum
+Csp5_scaffold_00005.g324|casiniprjna194557  Caenorhabditis_sinica
+CGOC_0000275601|cygoldprjeb498    Cylicostephanus_goldi
+arcanus-mkr-S_1590-0.0-mRNA-1|prarcaprjeb27334   Pristionchus_arcanus
+Acey_s0072.g653|anceylprjna231479 Ancylostoma_ceylanicum
+WBGene00139702|cabrenprjna20035   Caenorhabditis_brenneri
+HPOL_0000787701|hepolyprjeb15396  Heligmosomoides_polygyrus
+DCO_008622|dicoroprjdb3143   Diploscapter_coronatus
+FL83_01328|calateprjna248912 Caenorhabditis_latens
+HPLM_0000547301|haplacprjeb509    Haemonchus_placei
+fissidentatus-mkr-S40-3.15-mRNA-1|prfissprjeb27334    Pristionchus_fissidentatus
+DICVIV_07636|diviviprjna72587     Dictyocaulus_viviparus
+entomophagus-mkr-S570-0.22-mRNA-1|prentoprjeb27334    Pristionchus_entomophagus
+maxplancki-mkr-S100-2.104-mRNA-1|prmaxpprjeb27334     Pristionchus_maxplancki
+HCON_00151270|hacontprjeb506 Haemonchus_contortus
+exspectatus-mkr-S_1791-0.14-mRNA-1|prexspprjeb24288   Pristionchus_exspectatus
+japonicus-mkr-S267-0.26-mRNA-1|prjapoprjeb27334  Pristionchus_japonicus
+OESDEN_01101|oedentprjna72579     Oesophagostomum_dentatum
+MicoRS5524-ag_msk-S79-2.36-mRNA-1|mijapoprjeb27334    Micoletzkya_japonica
+WBGene00000047|caelegprjna13758   Caenorhabditis_elegans
+WBGene00092568|prpaciprjna12644   Pristionchus_pacificus
+SVUK_0001037501|stvulgprjeb531    Strongylus_vulgaris
+WR25_23289|dipachprjna280107 Diploscapter_pachys
+Sp34_X0071520|casp34prjdb5687     Caenorhabditis_inopinata
+NECAME_16202|neamerprjna72135     Necator_americanus
+mbelari.g26242|mebelaprjeb30104   Mesorhabditis_belari
+OTIPU.nOt.2.0.1.g01642|ostipuprjeb15512     Oscheius_tipulae
+nAv.1.0.1.g09901|acviteprjeb1697  Acanthocheilonema_viteae
+mayeri-mkr-S55-0.24-mRNA-1|prmayeprjeb27334 Pristionchus_mayeri
+NBR_0000568801|nibrasprjeb511     Nippostrongylus_brasiliensis
 
 
 
-control <- read.table("XQTL_CONTROL.merged.fst",header=F)
-control <- control[control$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-control <- dplyr::select(control, V1, V2, V11)
-colnames(control) <- c("CHR","POS","FST")
-control$LABEL <- "1. F2: Control"
-control$ROW_ID <- 1:nrow(control)
-colnames(control) <- c("CHR","POS","FST","LABEL","ROW_ID")
+# substitute names
+while read old new; do sed -i "s/${old}/${new}/" wbp_acr8.facopy; done < rename
 
-bz <- read.table("XQTL_BZ.merged.fst",header=F)
-bz <- bz[bz$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-bz <- dplyr::select(bz, V1, V2, V13)
-colnames(bz) <- c("CHR","POS","FST")
-bz$LABEL <- "2. F2: Benzimidazole"
-bz$ROW_ID <- 1:nrow(bz)
-colnames(bz) <- c("CHR","POS","FST","LABEL","ROW_ID")
-
-data <- dplyr::bind_rows(control,bz)
-
-lev <- read.table("XQTL_LEV.merged.fst",header=F)
-lev <- lev[lev$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-lev <- dplyr::select(lev, V1, V2, V13)
-colnames(lev) <- c("CHR","POS","FST")
-lev$LABEL <- "3. F2: Levamisole"
-lev$ROW_ID <- 1:nrow(lev)
-colnames(lev) <- c("CHR","POS","FST","LABEL","ROW_ID")
-
-data <- dplyr::bind_rows(data,lev)
-
-ivm <- read.table("XQTL_IVM.merged.fst",header=F)
-ivm <- ivm[ivm$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-ivm <- dplyr::select(ivm, V1, V2, V13)
-colnames(ivm) <- c("CHR","POS","FST")
-ivm$LABEL <- "4. F2: Ivermectin"
-ivm$ROW_ID <- 1:nrow(ivm)
-colnames(ivm) <- c("CHR","POS","FST","LABEL","ROW_ID")
-
-data <- dplyr::bind_rows(data,ivm)
-
-# make the XQTL plot
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-# genome wide signficance per sample
-data_gws <- data %>%
-    group_by(LABEL) %>%
-    summarise(GWS = mean(FST)+3*sd(FST))
-
-plot_b <- ggplot(data)+
-     geom_hline(data = data_gws, aes(yintercept=GWS), linetype="dashed",col="black")+
-     geom_point(aes(ROW_ID*5000, FST, colour = CHR, group=LABEL),size=0.1)+
-     ylim(0,0.1)+
-     labs(title="B",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8),limits = c(0,300e6))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
+# extract fasta sequences based on the new names to a new fasta
+while read old new; do samtools faidx wbp_acr8.facopy ${new}; done < rename > wb_cladeV_acr8.fa
 
 
-# combine the parental and XQTL plots into a single figure
-plot_a + plot_b + plot_layout(ncol=1, heights = c(1,4))
-ggsave("genomewide_fst_plots.pdf",useDingbats=FALSE,width=170,height=200,units="mm")
+module load mafft/7.407=1
 
-<!-- ```R
-R
+mafft --globalpair --maxiterate 16 --reorder "wb_cladeV_acr8.fa" > "wb_cladeV_acr8.aln"
+
+
+
+# removed
+entomophagus-mkr-S570-0.22-mRNA-1|prentoprjeb27334    Pristionchus_entomophagus - large insertion
+
+Pristionchus_exspectatus
+Cylicostephanus_goldi
+Dictyocaulus_viviparus
+Ancylostoma_caninum
+Caenorhabditis_sinica
+Caenorhabditis_brenneri
+
+
+library(ggmsa)
 library(ggplot2)
 
-# get data
-data <- read.table("XQTL_PARENTS.merged.fst",header=F)
-data <- data[data$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-
-data$LABEL <- "Parents: MHco3(ISE) vs MHco18(UGA)"
-# genome wide levels of significance
+data <- tidy_msa("wb_cladeV_acr8.aln", start = 225, end = 275)
+data[ data == "-" ] <- NA
 
 
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-fst_column <- data$V7
-
-genomewide_sig <- mean(fst_column)+(3*sd(fst_column))
-
-plot_a <- ggplot(data)+
-     geom_hline(yintercept=genomewide_sig, linetype="dashed",col="black")+
-     geom_point(aes(1:nrow(data)*5000, fst_column, colour = V1),size=0.1)+
-     ylim(0,1)+
-     labs(title="A",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
+colours <- c("A"="#CD2127","G"="#CD2127","I"="#CD2127","L"="#CD2127","P"="#CD2127","V"="#CD2127","F"="#AAC64B","W"="#AAC64B","Y"="#AAC64B","D"="#ED6823","E"="#ED6823","R"="#759CD1","H"="#759CD1","K"="#759CD1","S"="#F09AC1","T"="#F09AC1","C"="#EDAB20","M"="#EDAB20","N"="#283983","Q"="#283983","NA"="white")
 
 
-#ggsave("XQTL_parents_fst.pdf",useDingbats=FALSE,width=170,height=50,units="mm")
-
-```
-
-```R
-R
-library(ggplot2)
-
-# get data
-data <- read.table("XQTL_CONTROL.merged.fst",header=F)
-data <- data[data$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-data$LABEL <- "Control"
-# genome wide levels of significance
-
-
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-# Replicates
-# 1:4 = V11
-# 2:5 = V21
-# 3:6 = V29
-
-fst_column <- data$V11
-
-genomewide_sig <- mean(fst_column)+(3*sd(fst_column))
-
-plot_b <- ggplot(data)+
-     geom_hline(yintercept=genomewide_sig, linetype="dashed",col="black")+
-     geom_point(aes(1:nrow(data)*5000, fst_column, colour = V1),size=0.1)+
-     ylim(0,0.1)+
-     labs(title="B",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
-
-```
-```R
-R
-library(ggplot2)
-
-# get data
-data <- read.table("XQTL_BZ.merged.fst",header=F)
-data <- data[data$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-data$LABEL <- "Benzimidazole"
-# genome wide levels of significance
-
-
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-# Replicates
-# 1:5 = V13
-# 2:6 = V27
-# 3:7 = V39
-# 4:8 = V49
-
-
-fst_column <- data$V13
-
-genomewide_sig <- mean(fst_column)+(3*sd(fst_column))
-
-plot_c <- ggplot(data)+
-     geom_hline(yintercept=genomewide_sig, linetype="dashed",col="black")+
-     geom_point(aes(1:nrow(data)*5000, fst_column, colour = V1),size=0.1)+
-     ylim(0,0.1)+
-     labs(title="C",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
-```
-
-```R
-R
-library(ggplot2)
-
-# get data
-data <- read.table("XQTL_LEV.merged.fst",header=F)
-data <- data[data$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-data$LABEL <- "Levamisole"
-# genome wide levels of significance
-
-
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-# Replicates
-# 1:5 = V13
-# 2:6 = V27
-# 3:7 = V39
-# 4:8 = V49
-
-
-fst_column <- data$V13
-
-genomewide_sig <- mean(fst_column)+(3*sd(fst_column))
-
-plot_d <- ggplot(data)+
-     geom_hline(yintercept=genomewide_sig, linetype="dashed",col="black")+
-     geom_point(aes(1:nrow(data)*5000, fst_column, colour = V1),size=0.1)+
-     ylim(0,0.1)+
-     labs(title="D",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
-```
-
-## Ivermectin
-```R
-R
-library(ggplot2)
-
-# get data
-data <- read.table("XQTL_IVM.merged.fst",header=F)
-data <- data[data$V1!="hcontortus_chr_mtDNA_arrow_pilon",]
-data$LABEL <- "Ivermectin"
-# genome wide levels of significance
-
-
-chr_colours<-c("blue","cornflowerblue","blue","cornflowerblue","blue","cornflowerblue")
-
-# Replicates
-# 1:5 = V13
-# 2:6 = V27
-# 3:7 = V39
-# 4:8 = V49
-
-
-fst_column <- data$V13
-
-genomewide_sig <- mean(fst_column)+(3*sd(fst_column))
-
-plot_e <- ggplot(data)+
-     geom_hline(yintercept=genomewide_sig, linetype="dashed",col="black")+
-     geom_point(aes(1:nrow(data)*5000, fst_column, colour = V1),size=0.1)+
-     ylim(0,0.1)+
-     labs(title="E",x="Chromosome position (5 kbp window)", y="Genetic differentiation (Fst)")+
-     scale_color_manual(values=chr_colours)+
-     scale_x_continuous(breaks=seq(0,3e8,0.5e8))+
-     theme_bw()+theme(legend.position="none",text = element_text(size=10))+
-     facet_grid(LABEL~.)
-```
-
-# bring it together
-```R
-library(patchwork)
-plot_a + plot_b + plot_c + plot_d + plot_e + plot_layout(ncol=1)
-
-
-``` -->
+ggplot(data,aes(y=name, x=position)) +
+     geom_tile(aes(fill=character,alpha=0.5),col="grey",na.rm = TRUE) +
+     geom_text(aes(label=character),col="black")+
+     theme_minimal()+theme(legend.position = "none",text = element_text(size=10))+
+     labs(y="Species")+
+     scale_fill_manual(values = colours)+
+     coord_fixed()
 
 
 
 
-```
+
+
+Aliphatic #CD2127   A
+Aliphatic #CD2127   G
+Aliphatic #CD2127   I
+Aliphatic #CD2127   L
+Aliphatic #CD2127   P
+Aliphatic #CD2127   V
+Aromatic  #AAC64B    F
+Aromatic  #AAC64B    W
+Aromatic  #AAC64B    Y
+Acidic    #ED6823   D
+Acidic    #ED6823   E
+Basic     #759CD1   R
+Basic     #759CD1   H
+Basic     #759CD1   K
+Hydroxylic     #F09AC1   S
+Hydroxylic     #F09AC1   T
+Sulfur-containing   #EDAB20   C
+Sulfur-containing   #EDAB20   M
+Amidic    #283983   N
+Amidic    #283983   Q
+colours <- c("A"="#CD2127","G"="#CD2127","I"="#CD2127","L"="#CD2127","P"="#CD2127","V"="#CD2127","F"="#AAC64B","W"="#AAC64B","Y"="#AAC64B","D"="#ED6823","E"="#ED6823","R"="#759CD1","H"="#759CD1","K"="#759CD1","S"="#F09AC1","T"="#F09AC1","C"="#EDAB20","M"="#EDAB20","N"="#283983","Q"="#283983")
